349067f1680985377f15418bc2d180b9208843b8
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-03-02 03:22:28 +0600

    added welcome section updated

--- src/pages/Home/Home/Welcome/Welcome.jsx
+++ src/pages/Home/Home/Welcome/Welcome.jsx
@@ -16,22 +16,15 @@ const Welcome = () => {
             <section className="container mx-auto text-center px-2 md:px-3 lg:px-2.5">
                 <div
                     data-aos="fade-up"
-                    className="mx-auto w-full p-8 bg-gradient-to-r from-base-200 to-base-300 rounded-lg shadow-xl"
+                    className="mx-auto w-full p-8 bg-gradient-to-r from-blue-300 to-blue-500 rounded-lg shadow-xl"
                 >
-                    <h2 className="text-3xl md:text-5xl font-extrabold mb-4">
+                    <h2 className="text-3xl md:text-5xl text-black font-extrabold mb-4">
                         Welcome to LearnHive!
                     </h2>
-                    <p className="lg:max-w-4xl mx-auto md:text-lg mb-4 leading-relaxed">
+                    <p className="lg:max-w-4xl mx-auto md:text-lg text-black mb-4 leading-relaxed">
                         At LearnHive, we bridge the gap between educators, students, and institutions, creating a seamless platform for skill learning and class management.
                         Dive into a world of possibilities and make learning efficient, fun, and accessible for everyone.
                     </p>
-                    {/* <button
-                        data-aos="zoom-in"
-                        className="mt-4 bg-[#491558] hover:bg-[#6a1a84] text-white font-semibold py-3 px-8 rounded-full shadow-lg transition-all duration-300"
-                        onClick={() => console.log('Learn More button clicked')}
-                    >
-                        Learn More
-                    </button> */}
                 </div>
             </section>
         </div>
